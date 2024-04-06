4179be676e05f76e1f189b5f43a586e0498b2d0f
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-04-05 20:18:57 -0600

    :art: Empty braces on same line

--- Models/GreatRangeClass.cs
+++ Models/GreatRangeClass.cs
@@ -3,7 +3,5 @@ namespace StricterJudge.Models;
 internal sealed class GreatRangeClass : RangeClass
 {
     internal GreatRangeClass(string name, string display, MelonPreferences_Category category)
-        : base(name, display, category, 0, 80)
-    {
-    }
+        : base(name, display, category, 0, 80) { }
 }
--- Models/PerfectRangeClass.cs
+++ Models/PerfectRangeClass.cs
@@ -3,7 +3,5 @@ namespace StricterJudge.Models;
 internal sealed class PerfectRangeClass : RangeClass
 {
     internal PerfectRangeClass(string name, string display, MelonPreferences_Category category)
-        : base(name, display, category, 1, 50)
-    {
-    }
+        : base(name, display, category, 1, 50) { }
 }
