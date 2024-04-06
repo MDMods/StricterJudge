1f4ac286b5f2b72af421ea31a2c3f0858348edf3
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2024-04-05 20:36:25 -0600

    :art: Use fields instead of parameters

--- Models/RangeClass.cs
+++ Models/RangeClass.cs
@@ -7,6 +7,16 @@ internal abstract class RangeClass
 {
     private MelonPreferences_Entry<float> _localRange;
 
+    protected RangeClass(string name, string display, MelonPreferences_Category category,
+        int minRange, int maxRange)
+    {
+        Name = name;
+        DisplayText = display;
+        MinRange = minRange;
+        MaxRange = maxRange;
+        LoadEntry(category);
+    }
+
     private int MinRange { get; }
     private int MaxRange { get; }
 
@@ -20,46 +30,34 @@ internal abstract class RangeClass
     }
 
     private int RangeMs => LocalRange;
+
     internal Decimal RangeDec { get; private set; }
 
-    protected RangeClass(string name, string display, MelonPreferences_Category category,
-        int minRange, int maxRange)
-    {
-        Name = name;
-        DisplayText = display;
-        MinRange = minRange;
-        MaxRange = maxRange;
-        LoadEntry(category);
-    }
-
-    private void BaseRangeEntry(MelonPreferences_Category category, int minRange, int maxRange)
+    private void BaseRangeEntry(MelonPreferences_Category category)
     {
         _localRange = category.CreateEntry<float>(Name,
-            maxRange,
-            description: $"In ms, has to be between {minRange} and {maxRange}");
+            MaxRange,
+            description: $"In ms, has to be between {MinRange} and {MaxRange}");
     }
 
-    private void InitEntryValue(int minRange, int maxRange)
+    private void InitEntryValue()
     {
         var originalRange = LocalRange;
-        LocalRange = Mathf.Clamp(LocalRange, minRange, maxRange);
+        LocalRange = Mathf.Clamp(LocalRange, MinRange, MaxRange);
 
         RangeDec = (Decimal)RangeMs / 1000;
 
-        if (originalRange == LocalRange)
-        {
-            return;
-        }
+        if (originalRange == LocalRange) return;
 
         var warningMessage = $"Your selected range for {Name} is out of bounds.";
-        warningMessage += $"The value has to be between {minRange} and {maxRange}.";
+        warningMessage += $"The value has to be between {MinRange} and {MaxRange}.";
         MelonLogger.Warning(warningMessage);
     }
 
     private void LoadEntry(MelonPreferences_Category category)
     {
-        BaseRangeEntry(category, MinRange, MaxRange);
-        InitEntryValue(MinRange, MaxRange);
+        BaseRangeEntry(category);
+        InitEntryValue();
     }
 
     internal string GetRange() => RangeMs + "ms";
